8da1244f31f57462aec1fb0e1c1dee8ad29552c3
Charles Ukasoanya <c_ukasoanya@yahoo.com>
2022-11-16 08:46:58 +0100

    populate brander.tsx structure

--- brander-site/components/brander.tsx
+++ brander-site/components/brander.tsx
@@ -2,20 +2,49 @@ import React, { useState } from 'react';
 
 type Props = {};
 
+type ResultProps = {
+  snippet: string;
+  keywords: [];
+};
+
 const Brander = (props: Props) => {
   // set state parameters for the application
   const [prompt, setPrompt] = useState('');
+  const [snippet, setSnippet] = useState('');
+  const [keywords, setKeywords] = useState([]);
+  const [hasResult, setHasResult] = useState(false);
 
   //   api endpoint
   const ENDPOINT: string =
     'https://5z09phste9.execute-api.eu-west-2.amazonaws.com/prod/generate_snippet_and_keywords';
 
+  // handle returned datafrom fetch api
+  const onResult = (data: ResultProps) => {
+    setSnippet(data.snippet);
+    setKeywords(data.keywords);
+    setHasResult(true);
+  };
+
   //   function for handling submission of prompt
   const onSubmit = () => {
     fetch(`${ENDPOINT}?prompt=${prompt}`)
       .then((res) => res.json())
-      .then(console.log);
+      .then(onResult);
   };
+
+  //   Results element
+  let resultsElement = null;
+
+  //   populate resultsElement if hasResult is True
+  if (hasResult) {
+    resultsElement = (
+      <div>
+        Here are your results:
+        <div>Snippet: {snippet}</div>
+        <div>Keywords: {keywords.join(',')}</div>
+      </div>
+    );
+  }
 
   return (
     <>
@@ -31,6 +60,8 @@ const Brander = (props: Props) => {
         onChange={(e) => setPrompt(e.currentTarget.value)}
       ></input>
       <button onClick={onSubmit}>Submit</button>
+
+      {resultsElement}
     </>
   );
 };
